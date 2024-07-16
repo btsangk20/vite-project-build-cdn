18f4c337a2493fbb3d27eed91e6afb4f8d6a5a75
Bùi Tấn Sang <sang.t.bui@tgl-sol.com>
2024-07-16 17:05:53 +0700

    big update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-export * from "./utils/calculator.util";
+import { sum } from './utils/calculator.util';
+import { Button } from 'antd';
+
+window.process = {
+  env: {
+    NODE_ENV: 'production',
+  },
+};
+
+window.webGDL = {
+  sum,
+  Button,
+};
